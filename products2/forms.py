ccf4958d3ed94fe703bdc24efa92d77410104597
KauaLessa <kauajoestar@gmail.com>
2024-08-28 09:46:31 -0300

    account, product, cart, review features

---
+++ products2/forms.py
@@ -0,0 +1,27 @@
+import django.forms as forms
+from products2.models import Product
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class GetProductIdForm(forms.Form):
+    template_name = 'client/acc_management.html'
+
+    id = forms.IntegerField(label='ID')
+
+class ProductForm(forms.ModelForm):
+    template_name = 'client/acc_management.html'
+
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'price', 'image']
+
+class ProductReviewForm(forms.Form):
+    template_name = 'client/acc_management.html'
+
+    rating = forms.IntegerField(
+        validators = [
+            MinValueValidator(0),
+            MaxValueValidator(5)
+        ]
+    )
+    review = forms.CharField(widget=forms.Textarea)
+
